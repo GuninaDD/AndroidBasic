d4722fd5cf103f575224f604af93505f16fb1095
GuninaDD <dasha.gunina@g,ail.vom>
2020-03-12 22:15:54 +0300

    Android basic homework 1 done

---
+++ app/src/main/java/com/geekbrains/weather_application/Wthr_Activity.java
@@ -0,0 +1,13 @@
+package com.geekbrains.weather_application;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Wthr_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wthr);
+    }
+
+}
