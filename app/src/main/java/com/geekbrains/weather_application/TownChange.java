1d20609fb8006ac8d7319239ead9d79de4abb67f
GuninaDD <dasha.gunina@g,ail.vom>
2020-03-16 15:38:50 +0300

    Android basic homework 2 done

---
+++ app/src/main/java/com/geekbrains/weather_application/TownChange.java
@@ -0,0 +1,22 @@
+package com.geekbrains.weather_application;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class TownChange extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_town_change);
+
+    }
+
+}
